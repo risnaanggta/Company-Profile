773782249d6e066791df334297e819d15d537054
risna <672020237@student.uksw.edu>
2023-04-08 15:46:06 +0700

    kelima

--- query/booking.py
+++ query/booking.py
@@ -4,7 +4,7 @@ def query_booking(nama, notelp, tanggal, people, request):
     conn=connectdb.test_connection()
     if conn is not None:
         cur = conn.cursor()
-        cur.execute("INSERT INTO 672020237_pb_booking (nama, notelp, tanggal, people, request) VALUES (%s, %d, %d, %s, %s )", (nama, notelp, tanggal, people, request))
+        cur.execute("INSERT INTO _672020237_pb_booking (nama, notelp, tanggal, people, request) VALUES (%s, %d, %d, %s, %s )", (nama, notelp, tanggal, people, request))
         conn.commit()
         cur.close()
         return True
--- query/testimoni.py
+++ query/testimoni.py
@@ -4,7 +4,7 @@ def query_testimoni(nama, testimoni):
     conn=connectdb.test_connection()
     if conn is not None:
         cur = conn.cursor()
-        cur.execute("INSERT INTO 672020237_pb_testimoni (nama, testimoni) VALUES (%s, %s)", (nama, testimoni))
+        cur.execute("INSERT INTO _672020237_pb_testimoni (nama, testimoni) VALUES (%s, %s)", (nama, testimoni))
         conn.commit()
         cur.close()
         return True
