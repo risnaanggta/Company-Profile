33f2fdcd1790bd69d96211d6fa397a27311119af
risna <672020237@student.uksw.edu>
2023-04-10 12:26:44 +0700

    apaya

--- API/testimoni.py
+++ API/testimoni.py
@@ -2,52 +2,79 @@ import falcon, jwt
 from datetime import datetime, timedelta, timezone
 
 
-from query.testimoni import query_testimoni
+from query.testimoni import query_get_all_testimoni, query_add_testimoni, query_delete_testimoni, query_update_testimoni, query_get_testimoni_by_id
 
 
-class Testimoni:
+class ReadTestimoni:
+   def on_get(self, req, resp):
+        testimoni = query_get_all_testimoni()
+        if testimoni:
+            resp.media = {"testimoni": testimoni}
+            resp.status = falcon.HTTP_200
+        else:
+            resp.status = falcon.HTTP_404
+            resp.media = {'message': 'Testimoni not found'}
+
+class AddTestimoni:
    def on_post(self, req, resp):
-      id = req.media.get('id') 
-      nama = req.media.get('nama') 
-      testimoni = req.media.get('testimoni') 
-      
-      if not id or not nama or not testimoni:
-         resp.status = falcon.HTTP_BAD_REQUEST
-         return
-      
-      user = query_testimoni(id, nama, testimoni)
-
-      if user is True:
-         resp.status = falcon.HTTP_200
-         resp.media = {'message': 'Berhasil memberi testimoni'}
-      else:
-         resp.status = falcon.HTTP_401
-         resp.media = {'message': 'Gagal memberi testimoni'}
-
-   def on_delete(self, resp, id):
-      user = query_testimoni(id)
-      if user is True:
-         resp.status = falcon.HTTP_204
-         resp.media = {'message': 'Berhasil menghapus testimoni'}
-      else:
-         resp.status = falcon.HTTP_404
-         resp.media = {'message': 'Gagal menghapus testimoni'}
+        id = req.media.get('id') 
+        nama = req.media.get('nama') 
+        testimoni = req.media.get('testimoni') 
+        if not id or not nama or not testimoni:
+            resp.status = falcon.HTTP_BAD_REQUEST
+            return
+        testimoni = query_add_testimoni(id, nama, testimoni)
+        if testimoni is True:
+            resp.status = falcon.HTTP_200
+            resp.media = {'message': 'Add testimoni berhasil'}
+        else:
+            resp.status = falcon.HTTP_401
+            resp.media = {'message': 'Add testimoni gagal'}
 
 
-   def on_put(self, req, resp):
-      id = req.media.get('id') 
-      nama = req.media.get('nama') 
-      testimoni = req.media.get('testimoni') 
-         
-      if not id or not nama or not testimoni:
-         resp.status = falcon.HTTP_BAD_REQUEST
-         return
-      
-      updated = query_testimoni(id, nama, testimoni)
-      
-      if updated is True:
-         resp.status = falcon.HTTP_200
-         resp.media = {'message': 'Testimoni berhasil diperbarui'}
-      else:
-         resp.status = falcon.HTTP_404
-         resp.media = {'message': 'Gagal memperbarui testimoni'}
+class DeleteTestimoni:
+    def on_delete(self, req, resp):
+        nama = req.media.get('id')
+        if not nama:
+            resp.status = falcon.HTTP_BAD_REQUEST
+            return
+        testimoni = query_delete_testimoni(nama)
+        if testimoni is True:
+            resp.status = falcon.HTTP_200
+            resp.media = {'message': 'Delete testimoni berhasil'}
+        else:
+            resp.status = falcon.HTTP_401
+            resp.media = {'message': 'Delete testimoni gagal'}
+
+
+class UpdateTestimoni :
+    def on_put(self, req, resp):
+        id = req.media.get('id') 
+        nama = req.media.get('nama') 
+        testimoni = req.media.get('testimoni')
+       
+        testimoni = req.media.get('id')
+        if not id or not nama or not testimoni:
+            resp.status = falcon.HTTP_BAD_REQUEST
+            return
+        testimoni = query_update_testimoni(id, nama, testimoni)
+        if testimoni is True:
+            resp.status = falcon.HTTP_200
+            resp.media = {'message': 'Update testimoni berhasil'}
+        else:
+            resp.status = falcon.HTTP_401
+            resp.media = {'message': 'Update testimoni gagal'}
+
+class GetTestimoniById:
+    def on_post(self, req, resp):
+        id = req.media.get('id')
+        if not id:
+            resp.status = falcon.HTTP_BAD_REQUEST
+            return
+        testimoni = query_get_testimoni_by_id(id)
+        if testimoni:
+            resp.status = falcon.HTTP_200
+            resp.media = { 'testimoni': testimoni}
+        else:
+            resp.status = falcon.HTTP_401
+            resp.media = {'message': 'Product not found'}
