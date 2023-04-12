36a3fa1b1674990aff62575c9080f7f9daf8fa7c
risna <672020237@student.uksw.edu>
2023-04-12 22:52:18 +0700

    booking

--- main.py
+++ main.py
@@ -18,9 +18,32 @@ def service():
 def menu():
   return render_template("menu.html")
 
-@app.route("/booking")
+@app.route("/booking", methods=['GET', 'POST'])
 def booking():
-  return render_template("booking.html")
+    if request.method == 'POST':
+            # Ambil data produk dari form
+          nama = request.form['nama']
+          notelp = request.form['notelp']
+          tanggal = request.form['tanggal']
+          people = request.form['people']
+          request = request.form['request']
+            
+
+            # Buat data JSON yang berisi informasi produk yang akan ditambahkan
+          data = {"nama": nama, "notelp": notelp, "tanggal": tanggal, "people": people, "request": request }
+
+            # Kirim request POST ke API produk
+          try:
+              response = request.post('https://backend-risna-5zn7xh2gqq-et.a.run.app/booking', json=data)
+              if response.status_code == 201:
+                  print("Reservasi added successfully")
+              else:
+                  print("ERROR | Add product |", response.status_code)
+          except Exception as e:
+                print("ERROR | Add product |", e)
+    else:
+        print("ERROR | Add product |", "Invalid request method")
+    return render_template("booking.html")
 
 @app.route("/team")
 def team():
